058eb69657aa4e1229e0620cb75547544117a871
Blagoj <petrov.blagoj@uklo.edu.mk>
2025-07-11 14:25:20 +0200

    Initial commit

---
+++ src/components/accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+  {
+    id: 1,
+    question: "What is React?",
+    answer: "React is a JavaScript library for building user interfaces."
+  },
+  {
+    id: 2,
+    question: "What is a component in React?",
+    answer: "A component is a reusable piece of UI in a React application."
+  },
+  {
+    id: 3,
+    question: "What is JSX?",
+    answer: "JSX is a syntax extension for JavaScript that looks similar to HTML and is used with React to describe UI elements."
+  },
+  {
+    id: 4,
+    question: "What is a state in React?",
+    answer: "State is a built-in object that stores property values that belong to a component and can change over time."
+  }
+];
+
+export default data;
