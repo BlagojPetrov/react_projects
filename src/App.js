7cd8091b4e51ed410a18517d398645e81c28cbc7
Blagoj <petrov.blagoj@uklo.edu.mk>
2025-07-15 20:35:54 +0200

    added image_slider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import './App.css';
+import "./App.css";
+import ImageSlider from "./components/image_slider";
 // import Accordian from './components/accordian';
 // import RandomColor from './components/random-color';
-import StarRating from './components/star-rating';
+import StarRating from "./components/star-rating";
 
 function App() {
   return (
@@ -13,9 +14,13 @@ function App() {
       {/* <RandomColor /> */}
 
       {/* Star rating component */}
-      <StarRating />
+      {/* <StarRating /> */}
+
+      {/* Image slider component */}
+      <ImageSlider url={"https://picsum.photos/v2/list"} page={"1"} limit={"10"} />
     </div>
   );
 }
 
 export default App;
+
