caee1fd2a18d84c5091598da624d1b38e7df462e
Blagoj <petrov.blagoj@uklo.edu.mk>
2025-08-15 16:11:34 +0200

    Finish Food Recipe Project

--- food-recipe-app/src/context/index.jsx
+++ food-recipe-app/src/context/index.jsx
@@ -4,6 +4,10 @@ export const GlobalContext = createContext(null);
 
 export default function GlobalState({ children }) {
   const [searchParam, setSearchParam] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [recipeList, setRecipeList] = useState([]);
+  const [recipeDetailsData, setRecipeDetailsData] = useState(null);
+  const [favoritesList, setFavoritesList] = useState([]);
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -11,16 +15,51 @@ export default function GlobalState({ children }) {
       const res = await fetch(
         `https://forkify-api.herokuapp.com/api/v2/recipes?search=${searchParam}`
       );
+
       const data = await res.json();
-      console.log(data);
+
+      if (data?.data?.recipes) {
+        setRecipeList(data?.data?.recipes);
+        setLoading(false);
+        setSearchParam("");
+      }
     } catch (e) {
       console.log(e);
+      setLoading(false);
+      setSearchParam("");
     }
   }
 
+  function handleAddToFavorite(getCurrentItem) {
+    console.log(getCurrentItem);
+    let cpyFavoritesList = [...favoritesList];
+    const index = cpyFavoritesList.findIndex(
+      (item) => item.id === getCurrentItem.id
+    );
+
+    if (index === -1) {
+      cpyFavoritesList.push(getCurrentItem);
+    } else {
+      cpyFavoritesList.splice(index);
+    }
+    setFavoritesList(cpyFavoritesList);
+  }
+
+  console.log(favoritesList, "favoriteList");
+
   return (
     <GlobalContext.Provider
-      value={{ searchParam, setSearchParam, handleSubmit }}
+      value={{
+        searchParam,
+        loading,
+        recipeList,
+        setSearchParam,
+        handleSubmit,
+        recipeDetailsData,
+        setRecipeDetailsData,
+        handleAddToFavorite,
+        favoritesList,
+      }}
     >
       {children}
     </GlobalContext.Provider>
